4234c7e66dbdfa922203513d45846beed9f99749
unknown <hiba77moh@gmail.com>
2026-04-06 17:59:52 +0300

    updated the images

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -2,8 +2,6 @@
 import { educations } from "@/utils/data/educations";
 import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
-import lottieFile from '../../../assets/lottie/study.json';
-import AnimationLottie from "../../helper/animation-lottie";
 import GlowCard from "../../helper/glow-card";
 
 function Education() {
@@ -35,8 +33,14 @@ function Education() {
       <div className="py-8">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
           <div className="flex justify-center items-start">
-            <div className="w-3/4 h-3/4">
-              <AnimationLottie animationPath={lottieFile} />
+            <div className="w-full max-w-xl overflow-hidden rounded-3xl">
+              <Image
+                src="/image/education-image.png"
+                alt="Education illustration"
+                width={840}
+                height={840}
+                className="w-full h-auto object-cover"
+              />
             </div>
           </div>
 
